67775a4beb5c40a3245beb64c89cd4b13447315c
SAMAL04 <56217973+SAMAL04@users.noreply.github.com>
2019-10-07 14:53:18 +0200

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -2,11 +2,13 @@
 import math
 class bin_search:
     
-    def get_inp(a):   
+    def get_inp(c,a): 
+        #print(a)
         l = len(a)
         div_two = l/2
         ty_pe = type(div_two)
         list_in =[div_two,ty_pe]
+        #print(list_in)
         return list_in
 
     def find_centre(find_inp):
@@ -18,11 +20,9 @@ class bin_search:
             centre_element =cen
             print(centre_element)
         return centre_element
-b1 = bin_search()
+
 a = 2,3,5,7,8,10,34
-find_in = b1.get_inp(a)
-print(find_in)
-print(a)
-c = b1.ty_pe
-print(c)
-b1.find_centre(a, c)
+b1 = bin_search()
+#print(b1)
+find = b1.get_inp(a)
+print(find)
