c84f5cac0b49cb0ece0b4f8c24637147db9c4ec7
SAMAL04 <56217973+SAMAL04@users.noreply.github.com>
2019-10-07 14:57:21 +0200

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -2,7 +2,7 @@
 import math
 class bin_search:
     
-    def get_inp(c,a): 
+    def get_inp(self,a): 
         #print(a)
         l = len(a)
         div_two = l/2
@@ -11,18 +11,20 @@ class bin_search:
         #print(list_in)
         return list_in
 
-    def find_centre(find_inp):
-        cen = div_two
-        if ty_pe ==  float:
+    def find_centre(self,inp):
+        #print(inp)
+        cen = inp[0]
+        if inp[1] ==  float:
             centre_element =math.ceil(cen)
-            print(centre_element)
+            
         else:
             centre_element =cen
-            print(centre_element)
+            
         return centre_element
 
 a = 2,3,5,7,8,10,34
 b1 = bin_search()
 #print(b1)
 find = b1.get_inp(a)
-print(find)
+centre = b1.find_centre(find)
+print(centre)
