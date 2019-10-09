0bbafcefa016bb4094657ad778534ddaffd42472
SAMAL04 <56217973+SAMAL04@users.noreply.github.com>
2019-10-09 12:49:46 +0200

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -3,30 +3,33 @@ class bin_search():
     def mid(self,high,low,inp,a):
         
         while low != high:
-            b=a[low:high] 
-            print(b)
+            a=a[low:high] 
+            print(a)
             mid_val = int((high-low)/2)
             print(mid_val)
-            if b[mid_val] > inp:
-                print(b[mid_val])
+            if a[mid_val] > inp:
+                print(a[mid_val])
                 high = mid_val
                 low=0
-                #print("HIGH")
+                print("HIGH")
                 #print(high)
-            elif b[mid_val] < inp:
+            elif a[mid_val] < inp:
                 low = mid_val
                 high =len(a)
-                #print("LOW")
-                #print(low)
+                print("LOW")
+                print(low)
         
-            if b[mid_val] == inp:
+            if a[mid_val] == inp:
                 print("Value available")
                 break
+            #else:
+                #print("Not Available")
+                
          
         
 a = [1,2,3,4,5,6,7,8,9]
 low = 0
 high = len(a)
-inp  = 6
+inp  = 5
 b1=bin_search()
 b1.mid(high,low,inp,a)
