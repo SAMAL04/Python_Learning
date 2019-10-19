51596d1dc1281c459ac8b2ed42b2bc87d544eeb8
SAMAL04 <56217973+SAMAL04@users.noreply.github.com>
2019-10-19 16:33:36 +0530

    Create Max_Heap.py

---
+++ Max_Heap.py
@@ -0,0 +1,30 @@
+def add(a,insert):
+    List =[]
+    for x in a:
+        List.append(x)
+    List.append(insert)
+    return List   
+  
+def root(List,insert):   
+    s=0
+    for n in List:
+        s = s+1
+        if n == insert:
+            break
+            
+
+    ind =s-1
+    #insert = List[ind]
+    root1 = (ind-1)//2
+    root_ele = List[root1]
+    if insert > List[root1]:
+        List[root1] =insert
+        List[ind] = root_ele
+    print(List)
+    if List[0] < insert is not None:
+        root(List,insert)
+        print(List)
+list  = [9,7,6,4,2,3]
+insert = 10
+s = add(list,insert)
+root(s,insert)
