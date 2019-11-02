853b1f1919407e1da5bc0f28ecd82384d4fb2902
SAMAL04 <56217973+SAMAL04@users.noreply.github.com>
2019-11-02 05:49:16 +0530

    Create TopView.py

---
+++ TopView.py
@@ -0,0 +1,56 @@
+class NewNode:
+    def __init__(self, root):
+        self.val = root
+        self.left = None
+        self.right =None
+        self.hd = 0
+def topview(root):
+    if root is None:
+        return
+    else:
+        q=[]
+        q.append(root)
+        q.append(root.left)
+        q.append(root.right)
+        q.append(root.left.left)
+        q.append(root.left.right)
+        q.append(root.right.left)
+        q.append(root.right.right)
+        
+        m ={}
+        hd =0
+        root.hd = hd
+        hdv1 = []
+        while len(q):
+             root = q[0]
+             hd = root.hd
+             if hd not in m:
+                 r0 = {hd:root.val}
+                 
+             if (root.left):
+                    root.left.hd = hd-1  
+                    r1={root.left.hd:root.left.val}
+                    #m[root.left.hd]=root.left
+                    m.update(r1)
+             if (root.right):
+                    root.right.hd = hd+1      
+                    r2={root.right.hd:root.right.val}
+                    m.update(r2)
+                    #m[root.right.hd]=root.right
+             q.pop(0)
+
+        for i in sorted (m):
+            print(m[i])
+
+
+
+
+
+root = NewNode(1)
+root.left =NewNode(2)
+root.right =NewNode(3)
+root.left.left =NewNode(4)
+root.left.right =NewNode(5)
+root.right.left=NewNode(6)
+root.right.right =NewNode(7)
+topview(root)
